fd49d18fd4d92dbb8ed74270f95f1a76ecc93041
tumiterm <ifoliphant@forekinstitute.co.za>
2023-10-14 13:02:06 +0200

    (Forek Base) Clean Architecture with Example Models complete.

---
+++ ForekBase.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ForekBase.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForekBase.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<Question> Questions { get; set; }
+
+    }
+}
---
+++ ForekBase.Web/Controllers/QuestionsController.cs
@@ -0,0 +1,27 @@
+﻿using ForekBase.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForekBase.Web.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        public readonly IUnitOfWork _unitOfWork;
+
+        public QuestionsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var questions = _unitOfWork.Question.GetAll(includeProperties: "Survey");
+
+            return View(questions);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
